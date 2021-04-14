26234bd0701e12dbf84ff9683efe12b9e38caecc
0123456hahaha0123456 <ductran.tpc@gmail.com>
2021-04-14 12:14:55 +0300

    delete *.pyc

--- Crawler/process_reviews.py
+++ Crawler/process_reviews.py
@@ -36,6 +36,7 @@ def process(url):
         data[code] = path_file
         written_data_file = open('./data/listProduct.txt','w')
         json.dump(data, written_data_file)
+    print(data[code])
     return data[code]
 
 
@@ -43,6 +44,6 @@ def process(url):
 # 
 # url_product = 'https://www.amazon.com/Crucial-PC3-12800-Unbuffered-SODIMM-204-Pin/dp/B007B5S52C/?_encoding=UTF8&pd_rd_w=ZF57t&pf_rd_p=efec9084-7cd8-4122-9b80-914999cb4a82&pf_rd_r=3X1YP2CF71BYDBKWTSFY&pd_rd_r=1d386b7e-66e4-4794-8864-8ba8d72fe860&pd_rd_wg=g2vAz&ref_=pd_gw_cr_cartx'
 # url_product = 'https://www.amazon.com/SanDisk-128GB-Extreme-microSD-Adapter/dp/B07FCMKK5X/ref=pd_rhf_cr_s_pd_crcd_6?pd_rd_w=oIPrd&pf_rd_p=8019ba47-0a12-4976-b76b-5c932d60db6f&pf_rd_r=2NF0S495DBV1XXJ9XQYE&pd_rd_r=f9fad01d-18b9-4ff3-b2f0-e274a1c6d823&pd_rd_wg=HIXoP&pd_rd_i=B07FCMKK5X&psc=1'
-# url_product = 'https://www.amazon.com/Nintendo-Switch-Pro-Controller/dp/B01NAWKYZ0/?_encoding=UTF8&pd_rd_w=8rk4v&pf_rd_p=bb22ba69-5d1f-46eb-b0be-d8e50852ca56&pf_rd_r=KZ6EX5Y2D4SVRCTJV6V9&pd_rd_r=ea1eed6b-eeb3-4dd4-9170-817dab7724d7&pd_rd_wg=HAd4t&ref_=pd_gw_crs_zg_bs_468642'
+url_product = 'https://www.amazon.com/Nintendo-Switch-Pro-Controller/dp/B01NAWKYZ0/?_encoding=UTF8&pd_rd_w=8rk4v&pf_rd_p=bb22ba69-5d1f-46eb-b0be-d8e50852ca56&pf_rd_r=KZ6EX5Y2D4SVRCTJV6V9&pd_rd_r=ea1eed6b-eeb3-4dd4-9170-817dab7724d7&pd_rd_wg=HAd4t&ref_=pd_gw_crs_zg_bs_468642'
 # print(get_reviews(url_product))
-# process(url_product)
+process(url_product)
