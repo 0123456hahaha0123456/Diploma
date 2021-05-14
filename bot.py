15eb23c8d7a5ad367945fc10c8c71be5351b88c1
0123456hahaha0123456 <ductran.tpc@gmail.com>
2021-05-14 17:07:04 +0300

    fix bot

--- bot.py
+++ bot.py
@@ -2,11 +2,14 @@
 # -*- coding: utf-8 -*-
 # This program is dedicated to the public domain under the CC0 license.
 from telebot.credentials import bot_token
+
 from Crawler.process_reviews import process as reviewProcess
 from NLP.fact_extract import FaceExtraction
 import os
 import sys
 import time
+import json
+from json.decoder import JSONDecodeError
 from threading import Thread
 """
 Simple Bot to reply to Telegram messages.
@@ -32,30 +35,62 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 
+user_start = {}
+
+# Check if the user didnot start the bot
+def check(username):
+    if username not in user_start:
+        return False
+    return user_start[username]
+
+def write_userStart_file():
+    with open("./telebot/user_start.json", "w") as write_file:
+        json.dump(user_start, write_file)
+
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
 def start(update, context):
     """Send a message when the command /start is issued."""
-
-    update.message.reply_text('Hi! \n Welcome to reviews summary bot. Please input your product link to take overview !')
+    user_start[update.message.chat.username] = True
+    write_userStart_file()
+    update.message.reply_text('Hi! \n Welcome to reviews-summary bot. Please input your product link to take overview !')
 
 
 def help(update, context):
     """Send a messfactExtractage when the command /help is issued."""
-    update.message.reply_text('Help!')
+    start_text = "If you didnot start the bot, please type command '/start' to start! \n"
+    started_text = "If you already started the bot, please input product's link to get summary! \n"
+    update.message.reply_text(start_text + started_text)
+
+def end(update, context):
+    """End bot to this user when command /end is issued."""
+    user_start[update.message.chat.username] = False
+    write_userStart_file()
+    update.message.reply_text("Thank you for enjoying bot. See you later!")
 
 
 def echo(update, context):
     """Echo the user message."""
+    user = update.message.chat.username
+    if check(user) == False:
+        update.message.reply_text("Please start the bot!")
+        return
+     
+    update.message.reply_text("Please wait us to collect and process data for you! \n Loading... ")
     url = update.message.text
     file_data_path = reviewProcess(url)
     response = fact_extraction.factExtract(file_data_path)
-    # print(type(response))
     text = ""
     for item in response:
+        if len(response[item]) == 0:
+            continue
+        
         text = '<b>' + item + '</b>'
         text = text + '\n'
-        text = text + '\n'.join(response[item])
+        
+        for index, sent in enumerate(response[item]):
+            text = text + str(index+1) + '. '  + sent +  '\n'
+
         update.message.reply_text(text, parse_mode=ParseMode.HTML)
 
 
@@ -79,6 +114,7 @@ def main():
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("end", end))
 
     # on noncommand i.e message - echo the message on Telegram
     dp.add_handler(MessageHandler(Filters.text, echo))
@@ -96,5 +132,10 @@ def main():
 
 
 if __name__ == '__main__':
+    try:
+        with open("./telebot/user_start.json", "r") as read_file:
+            user_start = json.load(read_file)
+    except JSONDecodeError:
+        pass
     fact_extraction = FaceExtraction()
     main()
